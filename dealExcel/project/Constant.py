fda390a47f6759eeb2ee2483d10a5fe0a16ae963
itw_yuzx01 <1098034155@qq.com>
2021-07-06 20:12:44 +0800

    aa

--- dealExcel/project/Constant.py
+++ dealExcel/project/Constant.py
@@ -1,3 +1,5 @@
 # 定义现有的模板信息
 list_dic_model = [{'site': {'row': 5, 'col': 1}},
-                  {'site': {'row': 4, 'col': 1}}]
+                  {'site': {'row': 4, 'col': 1}},
+                  {'site': {'row': 3, 'col': 1}},
+                  {'site': {'row': 6, 'col': 1}}]
